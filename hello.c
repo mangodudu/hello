0247f293ff43c913c9e798667d2f256628336feb
mangodudu <55731762+mangodudu@users.noreply.github.com>
2019-10-15 14:41:16 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-CURL *curl
-CURLcode res;
+ CURL *curl;
+ CURLcode res;
 
-curl=curl_easy_init();
-if(cur1){
-curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
-res=curl_easy_perform(curl);
-if(res!=CURLE_OK)
-fprintf(stderr,"curl_easy_perform() failed:%s\n",
-curl_easy_strerror(res));
-curl_easy_cleanup(cur1);
-}
-return0;
+ curl=curl_easy_init();
+ if(cur1){
+   curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
+   res=curl_easy_perform(curl);
+   if(res!=CURLE_OK)
+     fprintf(stderr,"curl_easy_perform() failed:%s\n",
+           curl_easy_strerror(res));
+    curl_easy_cleanup(cur1);
+ }
+  return0;
 }
